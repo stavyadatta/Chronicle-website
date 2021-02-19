98284698753781fe68cd7efbb2c48e26be8d3a77
Stavya Datta <stavyad@student.unimelb.edu.au>
2021-02-20 00:42:49 +0530

    Added images and background to the site

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,17 +1,20 @@
 import React from "react";
-import "./MainPage.css"
+import Background_theme from './Images/background_theme.svg';
+import First_segment_graphic from './Images/first_segment_graphic.svg'
+import "./MainPage.css";
+
 
 
 function FirstSegment() {
-    let heading = 'Chronicle';
-    let subheading = 'Consumer Tech: Make it a reality'
     return (
         <div className="Main_page_first_segment">
             <div className = "FirstSegment_background">
-                
+                <img src={Background_theme} id="background_theme" alt="background_theme"/>
+                <img src={First_segment_graphic} id="first_segment_graphic" alt="background_theme"/>
+
             </div>
             <div>
-                <h1 className="Chronicle_heading"> {heading}</h1>
+                <h1 className="Chronicle_heading"> Chronicle</h1>
                 <h1 className = "Hackathon_detail"> Hackathon 2021</h1>
                 <h2 className = "Hackathon_topic">Consumer Tech: make it a reality</h2>
                 <h2 className = "Hackathon_date">15th – 21st of March</h2>
