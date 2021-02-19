279fb04b0bbe22acad8546d2eb2e83b9aab1c189
Stavya Datta <stavyad@student.unimelb.edu.au>
2021-02-19 21:53:30 +0530

    Added Main Page with hello-world

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+
+import MainPage from './MainPage.js';
+
+function MainPageRoutes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={MainPage}/>
+    </Switch>
+  )
+}
 
 function App() {
+  let route = <MainPageRoutes />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Router> {route} </Router>
   );
 }
 
