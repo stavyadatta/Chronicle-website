b2c4c2168eade246fa59c106cc1729650ddb40e9
Stavya Datta <stavyad@student.unimelb.edu.au>
2021-02-23 23:36:30 +0530

    Added second segment along with few buttons and details of the required

--- src/MainPage.js
+++ src/MainPage.js
@@ -6,6 +6,17 @@ import "./MainPage.css";
 // * mobile screen */ @media screen and (max-width: ..px) { /* all code for screen size goes here */ } /* laptop screen */ @media screen and (max-width: ..px) { /* all code for screen size goes here */ } 
 
 
+function MainPage(){
+    return (
+        <div className="home_page">
+            <FirstSegment/>
+            <TopButtons/>
+            <SecondSegment/>
+        </div>
+    );
+}
+
+
 function TopButtons() {
     return(
         <div>
@@ -29,9 +40,10 @@ function FirstSegment() {
                 <h1 className = "Hackathon_detail"> Hackathon 2021</h1>
                 <h2 className = "Hackathon_topic">Consumer Tech: make it a reality</h2>
                 <h2 className = "Hackathon_date">15th – 21st of March</h2>
-                <p className = "Hackathon_description">Chronicle is an annual hackathon hosted by the Google Developers Student Club - University of Melbourne (DSC Unimelb). This year, it’ll be held virtually and open to all university students around the world. 
-
-    The theme for this year is Consumer Tech: Make It a Reality.  Consumer Technology is any kind of technology that an individual, company or organization would purchase for completing tasks and meeting their needs. </p>
+                <p className = "Hackathon_description">Chronicle is an annual hackathon hosted by the Google Developers 
+                Student Club - University of Melbourne (DSC Unimelb). This year, it’ll be held virtually and open to all university students around the world. 
+                The theme for this year is Consumer Tech: Make It a Reality.  Consumer Technology is any 
+                kind of technology that an individual, company or organization would purchase for completing tasks and meeting their needs. </p>
                 <button className="sign_up"> Sign Up</button>
                 <button className='DSC_unimelb'>DSC Unimelb</button>
             </div>
@@ -39,13 +51,34 @@ function FirstSegment() {
     )
 }
 
-function MainPage(){
+function SecondSegment() {
     return (
-        <div className="home_page">
-            <FirstSegment/>
-            <TopButtons/>
+        <div className="Mainpage_second_segment">
+            <h1 className="Schedule_theme">Schedule</h1>
+            <button id="day_1">Day 1</button>
+            <button id="day_2">Day 2</button>
+            <button id="day_3">Day 3</button>
+            <SecondSegmentPlan/>
         </div>
-    );
+    )
+}
+
+function SecondSegmentBtns() {
+    return (
+        <div >
+            
+        </div>
+    )
+}
+
+function SecondSegmentPlan() {
+    return (
+        <div className="Plan_First_Day">
+            <h2> Friday, 19th March (AEDT)</h2>
+            <h4> Opening Ceremony            5:30PM</h4>
+            <h4> Start Hacking               6:30PM</h4>
+        </div>
+    )
 }
 
 export default MainPage;
